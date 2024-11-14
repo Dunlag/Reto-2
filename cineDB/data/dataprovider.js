f99fc22195da75c40546aba6420ed45c008fef76
Dunlag <fernando.pinilla85@gmail.com>
2024-11-14 13:16:18 +0100

    modificaciones

---
+++ cineDB/data/dataprovider.js
@@ -0,0 +1,15 @@
+var peliculas = require("./peliculas.json");
+
+function getAllPeliculas(){
+    return peliculas;
+}
+
+function getPelicula(id){
+    const item = peliculas.filter( (a) => { return(a.id==id)} );
+    return item[0];
+}
+
+module.exports = {
+    getAllPeliculas,
+    getPelicula
+}
--- cineDB/routes/index.js
+++ cineDB/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var datos = require("../data/dataprovider")
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/* router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+}); */
+
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+//pagina de login
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+//pagina de biblioteca
+
+router.get('/biblioteca', function(req, res, next) {
+  const imagenes = datos.getAllPeliculas();
+  res.render("biblioteca", {imagenes:imagenes});
+});
+
+//paginas especificas de cada pelicula
+
+router.get('/biblioteca/:id', function(req,res){
+  const pelicula = datos.getPelicula(req.params.id);
+  //res.send(plato);
+  res.render('pelicula',{pelicula:pelicula});
 });
 
 module.exports = router;
