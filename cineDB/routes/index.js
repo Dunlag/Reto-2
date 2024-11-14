dac75c04449aab279577823c667a383b28283b71
Dunlag <fernando.pinilla85@gmail.com>
2024-11-14 20:47:04 +0100

    login finalizado

--- cineDB/routes/index.js
+++ cineDB/routes/index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var datos = require("../data/dataprovider");
 
-var datos = require("../data/dataprovider")
+// Middleware para verificar si el usuario está autenticado
+function isAuthenticated(req, res, next) {
+    if (req.session.login) {
+        return next();  // Si está autenticado, sigue con la solicitud
+    }
+    res.redirect('/login');  // Si no está autenticado, redirige al login
+}
 
-/* GET home page. */
-/* router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}); */
-
+// Página de inicio
 router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-//pagina de login
-
+// Página de login
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
@@ -25,34 +27,29 @@ router.post('/login', function(req, res) {
   let user = datos.validateUser(usuario, password);
 
   if (user) {
-      console.log("Login exitoso:", user);
       req.session.login = true;
       req.session.user = user;
-      res.redirect("/biblioteca");
+      res.redirect("/biblioteca");  // Redirige a la página de administración
   } else {
-      console.log("Login fallido");
-      res.redirect("/login");
+      res.redirect("/login");  // Redirige de nuevo a login en caso de error
   }
 });
 
-
+// Cerrar sesión
 router.get('/logout', function(req, res) {
   req.session.destroy();
   res.redirect("/");
 });
 
-//pagina de biblioteca
-
-router.get('/biblioteca', function(req, res, next) {
+// Página de biblioteca protegida
+router.get('/biblioteca', isAuthenticated, function(req, res, next) {
   const imagenes = datos.getAllPeliculas();
   res.render("biblioteca", {imagenes:imagenes});
 });
 
-//paginas especificas de cada pelicula
-
-router.get('/biblioteca/:id', function(req,res){
+// Página de detalle de película protegida
+router.get('/biblioteca/:id', isAuthenticated, function(req,res){
   const pelicula = datos.getPelicula(req.params.id);
-  //res.send(plato);
   res.render('pelicula',{pelicula:pelicula});
 });
 
